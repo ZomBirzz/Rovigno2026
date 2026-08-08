d387b808b17da73f6f0014232a68e0b603f40178
birzzcachyos <ric.bersani@proton.me>
2026-08-08 16:32:40 +0200

    indice H calcolato

--- ANOVA/shannon/shannon.R
+++ ANOVA/shannon/shannon.R
@@ -4,15 +4,26 @@ dataset <- read.csv(file.choose(),header=T,sep=",", stringsAsFactors = T)
 attach(dataset)
 summary(dataset)
 
-for (variable in vector) {
-  
+length(dataset)
+
+# ciclo per H delle specie in copertura
+for (i in 1:450) {  
+  pop = as.numeric(dataset[i,6:25])
+  n = pop+1
+  N = sum(pop)
+  p = n/N
+  H = -sum(p*log(p))
+  print(H)
 }
-row = 1
-pop = dataset[row,1:19]
-n = pop+1
-N = sum(pop)
-p = n/N
-H = -sum(p*log(p))
-H
+
+# ciclo per H delle specie in n individui
+for (i in 1:450) {           
+  pop = dataset[i,26:47]
+  n = pop+1
+  N = sum(pop)
+  p = n/N
+  H = -sum(p*log(p))
+  print(H)
+}
 
 detach(dataset)
