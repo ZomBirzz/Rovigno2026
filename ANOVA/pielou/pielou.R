57e4233011e080943d0cad57d41433164049fc5d
birzzcachyos <ric.bersani@proton.me>
2026-08-17 15:32:49 +0200

    pielou calculated

---
+++ ANOVA/pielou/pielou.R
@@ -0,0 +1,10 @@
+rm(list=ls(all=TRUE))
+library(GAD)
+dataset <- read.csv(file.choose(),header=T,sep=",", stringsAsFactors = T)
+attach(dataset)
+summary(dataset)
+
+pielou = H1 / log(S)
+cat(pielou, file = "pielou.csv", sep = ",\n")
+
+detach(dataset)
